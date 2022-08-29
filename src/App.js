087b3d97456af6b6990a9cd11b6cd153291b7943
GiGson <shamatavagigucha@gmail.com>
2022-08-29 17:21:30 +0400

    adding images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import homeImage from './img/Group 1.png'
+import logo from './img/logoRedberry.png'
+
 function App() {
   return (
     <div className="App">
+
+      <div className="flexbox-container">
+
+      <img id='logo' src={logo} alt="redberryLogo" /> 
+      <img  src={homeImage} alt="welcome to homePage" />
+
+      </div>
+        
       
     </div>
   );
