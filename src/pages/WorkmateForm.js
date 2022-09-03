7e0d0dc2a646e3e4c1c1b2f59d550fc7c909026f
GiGson <shamatavagigucha@gmail.com>
2022-09-03 20:20:13 +0400

    structuring workmateform

--- src/pages/WorkmateForm.js
+++ src/pages/WorkmateForm.js
@@ -1,6 +1,28 @@
 const WorkmateForm = () => {
+    
     return ( 
-        <h1>თანამშრომლის ინფო</h1>
+        <div className="form-container workmate">
+            <form>
+
+            <label>სახელი</label>
+            <input type="text" name="" id="" placeholder="გრიშა"/>
+            <p></p>
+            
+            <label>გვარი</label>
+            <input type="text" name="" id="" placeholder="ბაგრატიონი"/>
+
+            <select name="" id=""><option value="">თიმი</option></select>
+
+            <select name="" id=""><option value="">პოზიცია</option></select>
+
+            <label>მეილი</label>
+            <input type="text" name="" id="" placeholder="grish666@redberry.ge"/>
+
+            <label>ტელეფონის ნომერი</label>
+            <input type="text" name="" id="" placeholder="+995 598 00 07 01"/>
+
+            </form>
+        </div>
      );
 }
  
