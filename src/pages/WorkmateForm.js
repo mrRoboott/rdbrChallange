eced7e66f3152c67f6036bda220bfad1d259cb1f
GiGson <shamatavagigucha@gmail.com>
2022-09-04 23:14:41 +0400

    adding styles to workmateform

--- src/pages/WorkmateForm.js
+++ src/pages/WorkmateForm.js
@@ -1,28 +1,39 @@
 const WorkmateForm = () => {
     
     return ( 
-        <div className="form-container workmate">
-            <form>
+        <form className="workmate-form">
+            <div className="form-element">
+                <div className="names-block">
+                    <label htmlFor="firstname">სახელი</label>
+                    <input type="text" id="firstname" placeholder="გრიშა"/>
+                    <span className="hint">მინიმუმ 2 სიმბოლო, ქართული ასოები</span>
+                </div>
 
-            <label>სახელი</label>
-            <input type="text" name="" id="" placeholder="გრიშა"/>
-            <p></p>
-            
-            <label>გვარი</label>
-            <input type="text" name="" id="" placeholder="ბაგრატიონი"/>
+                <div className="names-block lastname">
+                    <label htmlFor="lastname">გვარი</label>
+                    <input type="text" id="" placeholder="ბაგრატიონი"/>
+                    <span className="hint">მინიმუმ 2 სიმბოლო, ქართული ასოები</span>
+                </div>
+            </div>
+            <select name="" id="" ><option  value="">თიმი</option></select> 
+            <select name="" id="" ><option  value="">პოზიცია</option></select>
 
-            <select name="" id=""><option value="">თიმი</option></select>
+            <div className="form-element mail">
+                <label htmlFor="email" >მეილი</label>
+                <input type="email" id="email" placeholder="grish666@redberry.ge" />
+                <span className="hint">უნდა მთავრდებოდეს @redberry.ge-ით</span>
+            </div>
 
-            <select name="" id=""><option value="">პოზიცია</option></select>
+            <div className="form-element telephone">
+                <label htmlFor="telephone" >ტელეფონის ნომერი</label>
+                <input type="text" id="telephone" placeholder="+995 598 00 07 01" />
+                <span className="hint">უნდა აკმაყოფილებდეს ქართული მობ-ნომრის ფორმატს</span>
+            </div>
 
-            <label>მეილი</label>
-            <input type="text" name="" id="" placeholder="grish666@redberry.ge"/>
-
-            <label>ტელეფონის ნომერი</label>
-            <input type="text" name="" id="" placeholder="+995 598 00 07 01"/>
-
-            </form>
-        </div>
+            <div className="btn-container">
+            <button id="nextbtn">შემდეგი</button>
+            </div>
+        </form>
      );
 }
  
